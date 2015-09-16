2990c24d8c55179d15c44f7bb3993c9f8f21f5fb
JoanEricaCanada <joanerica.canada@gmail.com>
2015-09-16 16:00:00 +0800

    Revert "Modified Chapter3 Help.java for Exercise3-2"
    This reverts commit 4979b49b35162d5da5f526c32a5cbf5cc6c0205e.

--- Chapter3/Help.java
+++ Chapter3/Help.java
@@ -2,22 +2,12 @@ import java.io.*;
 
 public class Help {
 	public static void main(String args[]) throws IOException{
-		char choice, ignore;
-		do{
-			System.out.print("Help on:\n"
-					+ "\t1. if\n"
-					+ "\t2. switch\n"
-					+ "\t3. for\n"
-					+ "\t4. while\n"
-					+ "\t5. do-while\n"
-					+ "Choose one: ");
-			choice = (char)System.in.read();
-			do{
-				ignore = (char) System.in.read();
-				System.out.println(ignore);
-			}while(ignore != '\n');
-		}while(choice < '1' | choice > '5');
-		
+		System.out.print("Help on:\n"
+				+ "\t1. if\n"
+				+ "\t2. switch\n"
+				+ "Choose one: ");
+		char choice = (char)System.in.read();
+
 		switch(choice){
 		case '1':
 			System.out.println("The if:\n"
@@ -34,72 +24,34 @@ public class Help {
 					+ "\t\tbreak;\n"
 					+ "\t//...\n"
 					+ "}");
-			break;
-		case '3':
-			System.out.println("The for:\n"
-					+ "for(init; condition; iteration)\n"
-					+ "\tstatement;");
-			break;
-		case '4':
-			System.out.println("The while:\n"
-					+ "while(condition)\n"
-					+ "\t statement;");
-			break;
-		case '5':
-			System.out.println("The do-while:\n"
-					+ "do{\n"
-					+ "\tstatement;\n"
-					+ "}while(condition);");
+			break;	
 		default:
 			System.out.println("Selection not found.");
 		}
 		
 		// code from the book
-		do{
-			System.out.print("Help on:\n");
-			System.out.print("\t1. if\n");
-			System.out.print("\t2. switch\n");
-			System.out.print("\t3. for\n");
-			System.out.print("\t4. while\n");
-			System.out.print("\t5. do-while\n");
-			System.out.print("Choose one: ");
-			choice = (char)System.in.read();
-			do{
-				ignore = (char)System.in.read();
-			}while(ignore != '\n');
-		}while(choice < '1' | choice > '5');
+		System.out.println("Help on:\n");
+		System.out.println("\t1. if\n");
+		System.out.println("\t2. switch\n");
+		System.out.println("Choose one: ");
+		choice = (char)System.in.read();
 
 		switch(choice){
 		case '1':
-			System.out.print("The if:\n");
-			System.out.print("if(condition) statement;");
-			System.out.print("else");
-			System.out.print("\tstatement;");
-			break;
-		case '2':
-			System.out.print("The switch:\n");
-			System.out.print("switch(expression){");
-			System.out.print("\tcase constant:");
-			System.out.print("\t\tstatement sequence");
-			System.out.print("\t\tbreak;");
-			System.out.print("\t//...");
-			System.out.println("}");
-			break;
-		case '3':
-			System.out.println("The for:\n");
-			System.out.print("for(init; condition; iteration)");
+			System.out.println("The if:\n");
+			System.out.println("if(condition) statement;\n");
+			System.out.println("else\n");
 			System.out.println("\tstatement;");
 			break;
-		case '4':
-			System.out.println("The while:\n");
-			System.out.print("while(condition)");
-			System.out.print("\t statement;");
-			break;
-		case '5':
-			System.out.println("The do-while:\n");
-			System.out.print("do{");
-			System.out.print("\tstatement;");
-			System.out.println("}while(condition);");
+		case '2':
+			System.out.println("The switch:\n");
+			System.out.println("switch(expression){\n");
+			System.out.println("\tcase constant:\n");
+			System.out.println("\t\tstatement sequence\n");
+			System.out.println("\t\tbreak;\n");
+			System.out.println("\t//...\n");
+			System.out.println("}");
+			break;	
 		default:
 			System.out.println("Selection not found.");
 		}
