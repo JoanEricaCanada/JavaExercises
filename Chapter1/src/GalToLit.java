2ee7f57dd065bff76a17ab1e52e1081c815c0b95
JoanEricaCanada <joanerica.canada@gmail.com>
2015-09-16 13:42:32 +0800

    modified Chapter1 GalToInt.file for Exercise1-2

--- Chapter1/src/GalToLit.java
+++ Chapter1/src/GalToLit.java
@@ -2,13 +2,27 @@
 
 public class GalToLit {
 	public static void main(String args[]){
-		double gallons;
-		double liters;
+		double gallons, liters;
 		
-		gallons = 10;
+		for(gallons = 1; gallons <=100; gallons++){
+			liters = gallons * 3.7854;
+			System.out.println(gallons + " gallons is " + liters + " liters");
+			
+			if (gallons%10 == 0)
+				System.out.println();
+		}
 		
-		liters = gallons * 3.7854;
-		
-		System.out.println(gallons + " gallons is " + liters + " liters");
+		// code from the book
+		int counter = 0;
+		for(gallons = 1; gallons <=100; gallons++){
+			liters = gallons * 3.7854;
+			System.out.println(gallons + " gallons is " + liters + " liters");
+			
+			counter++;
+			if (counter == 10){
+				System.out.println();
+				counter= 0;
+			}
+		}
 	}
 }
