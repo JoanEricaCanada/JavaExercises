756773c2c0fe2eff6a94a49024300df13c90f22c
JoanEricaCanada <joanerica.canada@gmail.com>
2015-09-17 14:19:01 +0800

    Exercise6-2: Modified Queue.java and created QDemo2.java

--- Chapter6/Queue.java
+++ Chapter6/Queue.java
@@ -8,6 +8,25 @@ public class Queue {
 		putloc = getloc=0;
 	}
 	
+	Queue(Queue ob){
+		putloc = ob.putloc;
+		getloc = ob.getloc;
+		q = new char[ob.q.length];
+		
+		for (int i = getloc+1; i <= putloc; i++){
+			q[i] = ob.q[i];
+		}
+	}
+	
+	Queue(char a[]){
+		putloc = 0;
+		getloc = 0;
+		q = new char[a.length+1];
+		
+		for(int i = 0; i<a.length; i++)
+			put(a[i]);
+	}
+	
 	void put(char ch){
 		if (putloc ==q.length-1){
 			System.out.println(" - Queue is full");
