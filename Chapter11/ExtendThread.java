ae771fd32e75140bdb0162078ca4889419d2e7c9
JoanEricaCanada <joanerica.canada@gmail.com>
2015-09-21 16:24:29 +0800

    Created Chapter11 folder and Exercise11-1: MyThread.java, Extend.java

---
+++ Chapter11/ExtendThread.java
@@ -0,0 +1,18 @@
+
+public class ExtendThread {
+	public static void main(String args[]){
+		System.out.println("Main thread is starting.");
+		
+		MyThread mt = new MyThread("Child#1");
+		
+		for(int i = 0; i<50; i++){
+			System.out.print(" . ");
+			try{
+				Thread.sleep(100);
+			}catch(InterruptedException exc){
+				System.out.println("Main thread interrupted.");
+			}
+		}
+		System.out.println("Main thread ending.");
+	}
+}
